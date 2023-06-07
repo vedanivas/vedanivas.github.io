ec31ca5f96e15ef23d0a5f9a8a11b62aba8f85d4
vedanivas <veda.nivas@students.iiit.ac.in>
2023-06-08 02:39:49 +0530

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+function red()
+{
+  document.body.style.backgroundColor = "red"
+}
+function blue()
+{
+  document.body.style.backgroundColor = "blue"
+}
+function green()
+{
+  document.body.style.backgroundColor = "green"
+}
+function yellow()
+{
+  document.body.style.backgroundColor = "yellow"
+}
+function orange()
+{
+  document.body.style.backgroundColor = "orange"
+}
+function small()
+{
+  document.body.style.fontSize = "10px"
+}
+function medium()
+{
+  document.body.style.fontSize = "15px"
+}
+function large()
+{
+  document.body.style.fontSize = "20px"
+}
+
